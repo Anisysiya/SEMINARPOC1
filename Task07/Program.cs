208930dff62783536aaae5fef3351d9502c4d399
Anisya <anisiya@list.ru>
2023-07-09 10:20:54 +0300

    Исправили ошибки

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -6,8 +6,12 @@
 // 918 -> 8
 Console.Write("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+if (number<0)
+{
+    number*=-1;
+}
+if (number >=100 && number <= 999)
 
-if (number > 100 && number < 999)
 {
     int lastDigit = number % 10;
     Console.WriteLine($"Последняя цифра числа: {lastDigit}");
