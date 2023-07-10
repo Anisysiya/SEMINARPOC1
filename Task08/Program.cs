cc20ae92ef7f6941aea1057d6b0b4fa8fefd676c
Anisya <anisiya@list.ru>
2023-07-10 09:58:33 +0300

    Added Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу,
+//  которая на вход принимает число (N), 
+//  а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int res = 1;
+
+if (res == 1)
+
+{
+    res += 1;
+}
+
+while (res <= number)
+
+{
+    Console.WriteLine(res);
+    res += 2;
+}
