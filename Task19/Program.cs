0d4add2808c64018b94dda85ec3e2e573d782f51
Anisya <anisiya@list.ru>
2023-07-14 00:08:01 +0300

    Added Task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -5,3 +5,23 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+string fivenumber = Console.ReadLine();
+int dlina = fivenumber.Length;
+
+if (dlina == 5)
+{
+    if (fivenumber[0] == fivenumber[4] && fivenumber[1] == fivenumber[3])
+    {
+        Console.WriteLine($"{fivenumber} - число-палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{fivenumber} - не зеркально, это не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {fivenumber} - не является пятизначным числом по условию");
+}
