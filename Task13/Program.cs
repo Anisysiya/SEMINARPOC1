8a7fd0d9a88eb2fa1c3828570845f4b1686fe67f
Anisya <anisiya@list.ru>
2023-07-13 21:32:40 +0300

    Added Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит третью цифру
+//  заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число :");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 1000 || number < 100)
+{
+    Console.WriteLine("Не трехзначное число!!!");
+    return;
+}
+int GetSecond(int number)
+{
+    while (number >= 1000) number /= 100;
+    int d = number % 10;
+    return d;
+}
+Console.WriteLine(GetSecond(number));
+
