80965a1ce6071b7f9cd6ecccd7865a92ec8abb6a
Anisya <anisiya@list.ru>
2023-07-16 14:12:45 +0300

    Исправлено

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -3,9 +3,10 @@
 // числа в промежутке от -N до N.
 // 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
 // 2 -> " -2, -1, 0, 1, 2"
+
 Console.WriteLine("Введите целое положительное число");
 int number = Convert.ToInt32(Console.ReadLine());
-if(number<0)
+if(number<=0)
 {
    Console.WriteLine("Ошибка ввода"); 
 }
