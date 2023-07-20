cd5fd06f5d7fb7695e7e066dbf1016221f06e1f4
Anisya <anisiya@list.ru>
2023-07-20 19:44:00 +0300

    Added Task26

--- Task26/Program.cs
+++ Task26/Program.cs
@@ -5,7 +5,7 @@
 // 89126 -> 5
 
 Console.Write("Введите число: ");
-int number = int.Parse(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
 
 int count = GetCountDigits(number);
 Console.Write($"Количество цифр в числе {number} = {count}");
@@ -13,7 +13,6 @@ Console.Write($"Количество цифр в числе {number} = {count}")
 int GetCountDigits(int num)
 {
     if (num == 0) return 1;
-
     int counter = 0;
     while (num != 0)
     {
