cb4bbc8497f7be0517bce23da4026ea2d3624cfc
Anisya <anisiya@list.ru>
2023-07-20 20:18:19 +0300

    Added Task28

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -4,31 +4,19 @@
 // 4 -> 24
 // 5 -> 120
 
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
 
+int factorial = Factorial(number);
+Console.WriteLine($"Произведение чисел в диапазоне от 1 до {number} = {factorial}");
 
-
-int GetCountDitgitNumber(int number)
+int Factorial(int num)
 {
-    int count = 0;
-    while (number > 0)
+    int fact = 1;
+    
+    for (int i = 1; i <= num; i++)
     {
-        number = number / 10;
-        count++;
+        fact *= i; 
     }
-    return count;
-}
-
-long GetMultiplicationNumber(int number)
-{
-    if (number == 0)
-    {
-        Console.WriteLine("Нулик");
-        return 0;
-    }
-    long result = 1;
-    for (int i = 1; i <= number; i++)
-    {
-        result = result * i;
-    }
-    return result;
+    return fact;
 }
