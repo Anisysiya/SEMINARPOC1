ee79f16ea5e8ac135d6cae05724b8b898e6b6628
Anisya <anisiya@list.ru>
2023-07-21 19:49:35 +0300

    Added Task36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,6 +1,7 @@
-﻿// Задайте одномерный массив, заполненный
-//  случайными числами. Найдите сумму элементов,
-//   стоящих на нечётных позициях.
+﻿//1.Задайте одномерный массив, заполненный
+//  случайными числами. 
+//2.Найдите сумму элементов,
+//стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
@@ -8,30 +9,31 @@ Console.WriteLine("Введите размер массива  ");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
 
-FillArrayRandomNumbers(numbers);
+FillArrayRndNumbers(numbers);
 Console.WriteLine("массив: ");
 PrintArray(numbers);
 int sum = 0;
 
-for (int z = 1; z < numbers.Length; z+=2)
-    sum = sum + numbers[z];
+for (int i = 1; i < numbers.Length; i += 2)
+{ 
+    sum = sum + numbers[i];
+}
+Console.WriteLine($"всего {numbers.Length} чисел, сумма элементов cтоящих на нечётных позициях = {sum}");
 
-    Console.WriteLine($"всего {numbers.Length} чисел, сумма элементов cтоящих на нечётных позициях = {sum}");
-
-void FillArrayRandomNumbers(int[] numbers)
+void FillArrayRndNumbers(int[] numbers)
 {
-    for(int i = 0; i < numbers.Length; i++)
-        {
-            numbers[i] = new Random().Next(1,100);
-        }
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1, 100);
+    }
 }
 void PrintArray(int[] numbers)
 {
     Console.Write("[ ");
-    for(int i = 0; i < numbers.Length; i++)
-        {
-            Console.Write(numbers[i] + " ");
-        }
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
     Console.Write("]");
     Console.WriteLine();
 }
