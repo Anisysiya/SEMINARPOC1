96b1979406311c8fee4473e40f99363ea69e249a
Anisya <anisiya@list.ru>
2023-07-22 07:35:11 +0300

    Added Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 46 -> 101110
+// 13 -> 1101
+// 2 -> 10
+
+Console.Write("Введите десятичное число: ");
+int number = int.Parse(Console.ReadLine());
+
+// int result = DecToBinnary(number);
+// Console.WriteLine($"Число в двоичном представлении = {result}");
+
+
+// int DecToBinnary(int number)
+// {
+//     int numResult = 0;
+//     int decimalBase = 1;
+//     while (number > 0)
+//     {
+//         numResult += number % 2 * decimalBase;
+//         number /= 2;
+//         decimalBase *= 10;
+//     }
+//     return numResult;
+
+// }
+
+
+Console.WriteLine($"Число в двоичном представлении = {DecToBinnaryStr(number)}");
+
+string DecToBinnaryStr(int num)
+{
+    string numResult = string.Empty;
+    while (num > 0)
+    {
+        numResult = num % 2 + numResult;
+        num /= 2;
+    }
+    return numResult;
+
+}
