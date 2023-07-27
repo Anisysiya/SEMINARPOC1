0f1352761760b72c80b1a7d2cd24a58ba115c90b
Anisya <anisiya@list.ru>
2023-07-27 18:45:40 +0300

    Added Task47

---
+++ Task47/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n,
+//  заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.WriteLine("Задайте количество строк двумерного массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] twoDimArray = new double[m, n];
+
+Random rnd = new Random();
+
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        { Console.Write($"{matr[i, j]} "); }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] matr)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        { matr[i, j] = Convert.ToDouble(rnd.Next(-100, 100) / 10.0); }
+    }
+}
+FillArray(twoDimArray);
+Console.WriteLine();
+PrintArray(twoDimArray);
