527007a01299d865d60dd30f857c284482b3f10f
Anisya <anisiya@list.ru>
2023-07-29 01:05:08 +0300

    Added Task55

---
+++ Task55/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+        matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+    }
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+    Console.Write("[ ");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j], 4} ");
+    }
+    Console.WriteLine("]    ");
+    }
+}
+
+void ReplaceRowsColumns(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temp;
+        }
+    }
+}
+
+bool CheckSquareMatrix(int[,] matrix)
+{
+    return matrix.GetLength(0) == matrix.GetLength(1);
+}
+
+int[,] array2d = CreateMatrixRndInt(4, 3, 1, 9);
+if (!CheckSquareMatrix(array2d))
+{
+    Console.WriteLine("Матрица не квадратная, заменить не возможно");
+    return;
+}
+PrintMatrix(array2d);
+Console.WriteLine();
+ReplaceRowsColumns(array2d);
+PrintMatrix(array2d);
