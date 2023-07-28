63f3d997ba8c5fd52a574cf0b8af5c62d009ee9c
Anisya <anisiya@list.ru>
2023-07-28 14:51:54 +0300

    Added Task52

--- Task52/Program.cs
+++ Task52/Program.cs
@@ -6,19 +6,21 @@
 //8 4 2 4
 //Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-Random random = new Random();
-int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
+Random rnd = new Random();
+
+int[,] arr = new int[3, 4];
 
 for (int i = 0; i < arr.GetLength(0); i++)
 {
     for (int j = 0; j < arr.GetLength(1); j++)
     {
-        arr[i, j] = random.Next(1, 10);
+        arr[i, j] = rnd.Next(1, 10);
         Console.Write(arr[i, j] + " ");
     }
     Console.WriteLine();
 }
 Console.WriteLine("---------------------------");
+Console.WriteLine(arr.GetLength(0));
 for (int j = 0; j < arr.GetLength(1); j++)
 {
     double sum = 0;
@@ -26,6 +28,7 @@ for (int j = 0; j < arr.GetLength(1); j++)
     {
         sum += arr[i, j];
     }
-    Console.Write($"{ sum / arr.GetLength(0)} ");
+    Console.Write($"{sum / arr.GetLength(0)} ");
 }
-Console.ReadLine();
+
+Console.WriteLine();
