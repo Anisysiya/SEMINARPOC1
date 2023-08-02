b77c9cfb192822e1416830c103572bac395dbf9e
Anisya <anisiya@list.ru>
2023-08-02 21:00:19 +0300

    Added Task53

--- Task53/Program.cs
+++ Task53/Program.cs
@@ -3,17 +3,16 @@
 // массива.
 
 int[,] CreateMatrixyRndInt(int rows, int colums, int min, int max) // Для двумерного массива в сигнатуре указываются строки rows и столбцы colums.
-
 {
     int[,] matrix = new int[rows, colums];              // Создали двумерный массив заполненный нулями. rows=3, colums=4.
 
-    Random rnd = new Random();                          //Создали объект Random.
+    Random rnd = new Random(); 
+                             //Создали объект Random.
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
             matrix[i, j] = rnd.Next(min, max + 1);       // +1 потому что в методе Next интервал не включен
-
         }
     }
     return matrix;
