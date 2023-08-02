e331964c1d3cf316db5094cad1ddf9d4a31760af
Anisya <anisiya@list.ru>
2023-08-02 21:01:58 +0300

    Added Task55

--- Task55/Program.cs
+++ Task55/Program.cs
@@ -12,22 +12,22 @@ int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-        matrix[i, j] = rnd.Next(min, max + 1);
+            matrix[i, j] = rnd.Next(min, max + 1);
         }
     }
     return matrix;
-    }
+}
 
 void PrintMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
-    Console.Write("[ ");
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-        Console.Write($"{matrix[i, j], 4} ");
-    }
-    Console.WriteLine("]    ");
+        Console.Write("[ ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]    ");
     }
 }
 
