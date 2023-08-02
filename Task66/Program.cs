d5ac98f77038e3eac8f297fa779fd9a69e123def
Anisya <anisiya@list.ru>
2023-08-02 23:58:24 +0300

    Added Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+//  которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите N");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите M");
+int m = int.Parse(Console.ReadLine());
+
+int result = 0;
+Console.WriteLine(Sum(n, m));
+
+int Sum(int n, int m)
+{
+    if (m <= n)
+        return n;
+    return m + Sum(n, m - 1);
+}
+Console.WriteLine("Программа вычисления функции Аккермана");
